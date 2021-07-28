7ebd430e1cf7814675366072eb16f918cc92f7b2
Tim Green <timgreen99@gmail.com>
2021-07-28 12:56:03 -0600

    UNUserNotifications in AppDelegate

--- MedicationManager/MedicationManager/Resources/AppDelegate.swift
+++ MedicationManager/MedicationManager/Resources/AppDelegate.swift
@@ -7,46 +7,73 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (authorized, error) in
+            if let error = error {
+                print("There was an error requesting authorization to use notifications. Error: \(error)")
+            }
+            
+            if authorized {
+                UNUserNotificationCenter.current().delegate = self
+                self.setNotificationCategories()
+                print("✅ User authorized notifications.")
+                
+            } else {
+                
+                print("❌ The user declined the use of notifications.")
+                
+            }
+        }
         return true
     }
-
+    
+    private func setNotificationCategories() {
+        let markTakenAction = UNNotificationAction(identifier: StringConstants.markTakenNotificationActionIdentifier, title: StringConstants.accept, options: UNNotificationActionOptions(rawValue: 0))
+        
+        let ignoreAction = UNNotificationAction(identifier: StringConstants.ignoreNotificationActionIdentifier, title: StringConstants.ignore, options: UNNotificationActionOptions(rawValue: 0))
+        
+        let medicationActionCategory = UNNotificationCategory(identifier: StringConstants.notificationCategoryIdentifier, actions: [markTakenAction, ignoreAction], intentIdentifiers: [], hiddenPreviewsBodyPlaceholder: "", options: .customDismissAction)
+        
+        UNUserNotificationCenter.current().setNotificationCategories([medicationActionCategory])
+    }
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
     // MARK: - Core Data stack
-
+    
     lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
-        */
+         */
         let container = NSPersistentContainer(name: "MedicationManager")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
+                
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
@@ -60,9 +87,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         })
         return container
     }()
-
+    
     // MARK: - Core Data Saving support
-
+    
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
@@ -76,6 +103,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
+    
 }
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+} // End of extension
 
